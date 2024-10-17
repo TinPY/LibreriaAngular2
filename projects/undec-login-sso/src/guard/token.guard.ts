37968ba7a054bce0277702df002f46e5452581bd
Martin Paez Yañez <martinpy@gmail.com>
2024-10-17 17:43:32 -0300

    [guard]: se agrega token guard

---
+++ projects/undec-login-sso/src/guard/token.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivateFn } from '@angular/router';
+import * as jwt_decode from 'jwt-decode';
+
+let redirectCount = 0;
+
+export const tokenGuard: CanActivateFn = (route, state) => {
+  const localStorageToken = localStorage.getItem('undec-token');
+  if (localStorageToken) {
+    return true;
+  }
+
+  const tokenQueryParams = route.queryParamMap.get('idToken');
+  if (tokenQueryParams) {
+    const decodedToken: { [key: string]: string } = jwt_decode.jwtDecode(tokenQueryParams);
+    localStorage.setItem('user', decodedToken['email']);
+    localStorage.setItem('undec-token', tokenQueryParams);
+    return true;
+  }
+
+  if (redirectCount === 0) {
+    const locationBase64 = window.btoa(unescape(encodeURIComponent(window.location.origin + "#/home")))
+    const authUrl = "http://localhost:8899/auth/login?state=" + locationBase64
+    redirectCount = 1;
+    window.location.assign(authUrl);
+  }
+
+  return false;
+};
